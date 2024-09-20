b20338e79a539115b217017de7ea1234f1da76a5
Abhay <abhay@keyvalue.systems>
2024-09-20 17:26:26 +0530

    fix: UI changes

--- src/components/ArchitectureDiagram.tsx
+++ src/components/ArchitectureDiagram.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 interface ArchitectureDiagramProps {
-  monitorAndLogging: string[];
-  externalServices: string[];
-  storage: string[];
-  frontEnd: string[];
-  backEndServices: string[];
-  additionalInfo: string[]; // New prop for additional info
+  monitorAndLogging: Item[];
+  externalServices: Item[];
+  storage: Item[];
+  frontEnd: Item[];
+  backEndServices: Item[];
+}
+
+type  Item = {
+  id: number,
+  imageUrl: string,
+  label: string,
 }
 
 const ArchitectureDiagram: React.FC<ArchitectureDiagramProps> = ({
@@ -15,18 +20,17 @@ const ArchitectureDiagram: React.FC<ArchitectureDiagramProps> = ({
   storage,
   frontEnd,
   backEndServices,
-  additionalInfo, // Destructure the new prop
 }) => {
   const renderMonitoringAndLogging = () => {
     return (
-      <div className="row-span-3 col-span-1 border-2 border-dotted border-gray-400 items-center">
-        <h3 className="font-bold">Monitor and Logging</h3>
+      <div className="row-span-3 col-span-1 border-2 border-dotted border-gray-400 p-4 w-full h-full items-center">
+        <h3 className="font-bold mb-6">Monitor and Logging</h3>
         {monitorAndLogging.map((tool, index) => (
           <div
             key={index}
-            className="border-2 border-dotted border-gray-400 m-1 p-2"
+            className="bg-blue-300 m-1 p-2 h-16 mb-6"
           >
-            {tool}
+            {tool?.label}
           </div>
         ))}
       </div>
@@ -35,47 +39,49 @@ const ArchitectureDiagram: React.FC<ArchitectureDiagramProps> = ({
 
   const renderExternalServices = () => {
     return (
-      <div className=" border-2 border-dotted border-gray-400 row-span-1 col-span-3 items-center p-5">
-        <div className=" flex p-5">
-          <h3 className="font-bold">External Services</h3>
+      <div className=" border-2 border-dashed border-gray-400 row-span-1 col-span-3 items-center p-5 w-full">
+          <h3 className="font-bold mb-6">External Services</h3>
+          <div className="flex flex-wrap justify-around align-middle">
           {externalServices.map((service, index) => (
             <div
               key={index}
-              className="border-2 border-dotted border-gray-400 m-1 p-2 w-36"
+              className="bg-red-300 m-1 p-2 w-36"
             >
-              {service}
+              {service?.label}
             </div>
           ))}
-        </div>
+          </div>
       </div>
     );
   };
 
   const renderStorage = () => {
     return (
-      <div className="row-span-1 col-span-2 border-2 border-dotted border-gray-400  items-center">
-        <h3 className="font-bold">Storage</h3>
+      <div className="row-span-1 col-span-2 border-2 border-dashed border-gray-400 w-full h-full p-4 items-center">
+        <h3 className="font-bold mb-4">Storage</h3>
+        <div className="flex flex-row">
         {storage.map((storageItem, index) => (
           <div
             key={index}
-            className="border-2 border-dotted border-gray-400 m-1 p-2"
+            className="bg-gray-400 m-1 p-2 h-24 w-32 mb-6 mr-10 justify-center text-center content-center"
           >
-            {storageItem}
+            {storageItem?.label}
           </div>
         ))}
+        </div>
       </div>
     );
   };
   const renderFrondEnd = () => {
     return (
-      <div className="row-span-1 col-span-1 border-2 border-dotted border-gray-400  items-center">
-        <h3 className="font-bold">Front-end</h3>
+      <div className="row-span-1 col-span-1 border-2 border-dashed border-gray-400 w-full h-full p-4 items-center">
+        <h3 className="font-bold mb-4">Front-end</h3>
         {frontEnd.map((tech, index) => (
           <div
             key={index}
-            className="border-2 border-dotted border-gray-400 m-1 p-2"
+            className="bg-purple-300 m-1 p-2 h-16 mb-6"
           >
-            {tech}
+            {tech?.label}
           </div>
         ))}
       </div>
@@ -84,49 +90,47 @@ const ArchitectureDiagram: React.FC<ArchitectureDiagramProps> = ({
 
   const renderBackendServices = () => {
     return (
-      <div className="row-span-2 col-span-1 border-2 border-dotted border-gray-400 items-center justify-center">
-        <h3 className="font-bold">Back-end Services</h3>
+      <div className="row-span-2 col-span-1 border-2 border-dashed border-gray-400 w-full h-full p-4 items-center justify-center">
+        <h3 className="font-bold mb-6">Back-end Services</h3>
+        <div className="flex flex-wrap justify-around align-middle h-ful">
         {backEndServices.map((service, index) => (
           <div
             key={index}
-            className="border-2 border-dotted border-gray-400 m-1 p-2"
+            className="bg-emerald-300 m-10 mt-0 p-2 h-24"
           >
-            {service}
+            {service?.label}
           </div>
         ))}
+        </div>
       </div>
     );
   };
 
   return (
-    <div className="grid grid-cols-[15%_50%_20%_15%] grid-rows-[25%_50%_25%] gap-1 h-full bg-red-600">
-      <div className="bg-blue-500 flex items-center justify-center h-full">
-        1
+    <div className="grid grid-cols-[15%_50%_15%_20%] grid-rows-[25%_50%_25%] gap-1 h-full">
+      <div className="flex items-center justify-center h-full p-4">
       </div>
-      <div className="bg-blue-500 flex items-center justify-center h-full">
-        {renderExternalServices()}
+      <div className="flex justify-center h-full p-4 items-end">
+        {externalServices?.length> 0 && renderExternalServices()}
       </div>
-      <div className="bg-blue-500 flex items-center justify-center h-full col-span-2">
-        3
+      <div className="flex items-center justify-center h-full  p-4 col-span-2 ">
       </div>
-      <div className="bg-blue-500 flex items-center justify-center h-full row-span-2">
-        {renderMonitoringAndLogging()}
+      <div className="flex items-center justify-center h-full  p-4 row-span-2">
+        {monitorAndLogging?.length>0 && renderMonitoringAndLogging()}
       </div>
-      <div className="bg-blue-500 flex items-center justify-center h-full">
-        {renderBackendServices()}
+      <div className="flex items-center justify-center h-full p-4">
+        {frontEnd?.length>0 && renderBackendServices()}
       </div>
-      <div className="bg-blue-500 flex items-center justify-center h-full">
-        {renderFrondEnd()}
+      <div className="flex items-center justify-center h-full p-4">
+        {frontEnd?.length>0 && renderFrondEnd()}
       </div>
-      <div className="bg-blue-500 flex items-center justify-center h-full">
-        8
+      <div className="flex items-center justify-center h-full p-4">
       </div>
 
-      <div className="bg-blue-500 flex items-center justify-center h-full">
-        {renderStorage()}
+      <div className="flex items-center justify-center h-full p-4">
+        {storage?.length>0 && renderStorage()}
       </div>
-      <div className="bg-blue-500 flex items-center justify-center h-full col-span-2">
-        11
+      <div className="flex items-center justify-center h-full p-4 col-span-2">
       </div>
     </div>
   );
--- src/containers/homeScreen/HomeScreen.tsx
+++ src/containers/homeScreen/HomeScreen.tsx
@@ -1,15 +1,101 @@
 import ArchitectureDiagram from "../../components/ArchitectureDiagram";
 
+const monitorAndLogging = [
+  {
+    id: 1,
+    imageUrl: 'image1',
+    label: 'Tool1',
+  },
+  {
+    id: 2,
+    imageUrl: 'image2',
+    label: 'Tool2',
+  },
+];
+
+const externalServices = [
+  {
+    id: 1,
+    imageUrl: 'image1',
+    label: 'Service1',
+  },
+  {
+    id: 2,
+    imageUrl: 'image2',
+    label: 'Service2',
+  },
+];
+
+const storage = [
+  {
+    id: 1,
+    imageUrl: 'image1',
+    label: 'Storage1',
+  },
+  {
+    id: 2,
+    imageUrl: 'image2',
+    label: 'Storage2',
+  },
+];
+
+const frontEnd = [
+  {
+    id: 1,
+    imageUrl: 'image1',
+    label: 'React Native'
+  },
+  {
+    id: 2,
+    imageUrl: 'image2',
+    label: 'React',
+  },
+];
+
+const backEndServices = [
+  {
+    id: 1,
+    imageUrl: 'image1',
+    label: 'Microservice1',
+  },
+  {
+    id: 2,
+    imageUrl: 'image2',
+    label: 'Microservice2',
+  },
+  {
+    id: 3,
+    imageUrl: 'image3',
+    label: 'Microservice3',
+  },
+  {
+    id: 4,
+    imageUrl: 'image4',
+    label: 'Microservice4',
+  },
+  {
+    id: 5,
+    imageUrl: 'image5',
+    label: 'Microservice5',
+  },
+  {
+    id: 6,
+    imageUrl: 'image6',
+    label: 'Microservice6',
+  },
+
+];
+
+
 const HomeScreen = () => {
   return (
     <div className="flex-1 h-full p-20">
       <ArchitectureDiagram
-        monitorAndLogging={["Tool1", "Tool2"]}
-        externalServices={["Service1", "Service2"]}
-        storage={["Storage1", "Storage2"]}
-        frontEnd={["React Native", "React"]}
-        backEndServices={["Microservice1", "Microservice2"]}
-        additionalInfo={[]}
+        monitorAndLogging={monitorAndLogging}
+        externalServices={externalServices}
+        storage={storage}
+        frontEnd={frontEnd}
+        backEndServices={backEndServices}
       />
     </div>
   );
